09d5ef2b9164d1ba6a8d31ffc9409d86ed104d15
fargendo <landrum287@hotmail.com>
2020-03-25 02:13:05 -0500

    rm app.css and fix prod. port

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 import Chat from './components/Chat';
 
 import { makeStyles } from '@material-ui/core/styles';

d3eda59177eb5ef5cf5c009b1e8af2cef376a419
fargendo <landrum287@hotmail.com>
2020-04-19 16:49:31 -0500

    secure url

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -53,7 +53,13 @@ const Chatbox = (props) => {
 		let lastMessages = [];
 		if (!messages.length) {
 			console.log('Fetching messages from DB');
-			fetch(URL)
+			fetch(URL, {
+				method: 'GET',
+				headers: {
+					authorization:
+						'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkhEckMtZ3VuTTVyNzlVV3h1cFBRRiJ9.eyJpc3MiOiJodHRwczovL2RtY2NoYXQuYXV0aDAuY29tLyIsInN1YiI6IjFhMmZmRlltejFkNjhXZG5MWjEyTWoxVVA4RnVrMzFDQGNsaWVudHMiLCJhdWQiOiJodHRwOi8vZG1jY2hhdC5jb20iLCJpYXQiOjE1ODczMjU0NTksImV4cCI6MTU4NzQxMTg1OSwiYXpwIjoiMWEyZmZGWW16MWQ2OFdkbkxaMTJNajFVUDhGdWszMUMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.IUMQ6F_ZkbWeMuayzG56Sbz4iC-YUlDoxVxAUzN8AUeteq4Lzx4xtn8Tp03XX8Xwd5IYzLjey8Bfqz5r7oflVhcW_2aYxl3zsxQDU6p9oP1-v2UBAa3AXMEnrN9k7-qJp_jLjkPOoqX4RlJHuP6aT3u6kGOrKlcDCK-kFakz6rq7rt68jkUUfFSME6QKFvohCWGoQAUNcjKRLyoaYwUj7FBcdDGygsGz4YgfLo_JpFgQ7vw2qv_dm_EppT53gVfe51jwXN4O-gOdspW08AEepJtlFrTUpV_GRXaTi4wRfhmtkeN4K7TT5f0b9KIkC1XrVAtc2lD9i5Jo6z_aLt32Ww',
+				},
+			})
 				.then((response) => response.json())
 				.then((data) => {
 					data.forEach((message) => {
